0cbe06b27657ce3c49d66a28ce7c91b120d2a4c2
binhduong160199 <if22b055@technikum-wien.at>
2025-06-08 17:59:03 +0200

    final initial project (#4)

--- DatawareHouse.API/Migrations/20250608153508_InitialCreate.cs
+++ DatawareHouse.API/Migrations/20250608153508_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace DatawareHouse.API.Migrations
+namespace DataWarehouse.API.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
